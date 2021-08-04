1db75a039f48ea7836ca34b235cce95b719471cf
lucasruiz12 <lucaschatoruiz@gmail.com>
2021-08-04 16:04:26 -0300

    back 99%, check validations in post, starting front

--- api/src/controllers/activity.js
+++ api/src/controllers/activity.js
@@ -1,7 +1,7 @@
 const { Country, Activity } = require('../db');
 
 const postActivity = async (req, res) => {
-    const { name, difficulty, duration, season, id } = req.body;
+    const { name, difficulty, duration, season, idCountry } = req.body;
     const validate = await Activity.findOne({
         where: {
             name: name
@@ -16,16 +16,16 @@ const postActivity = async (req, res) => {
         });
         const checkCountry = await Country.findAll({
             where: {
-                id: id
+                id: idCountry
             }
         });
         const showActivity = await createActivity.addCountries(checkCountry);
-
+        
         return res.send(showActivity);
-    }
+    } 
     const checkCountry = await Country.findAll({
         where: {
-            id: id
+            id: idCountry
         }
     });
     const showActivity = await createActivity.addCountries(checkCountry);
